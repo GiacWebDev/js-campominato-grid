2e9ae9508f98cedbb00bf0e935066f6c1eafe3ed
GiacWebDev <giacobechini@gmail.com>
2023-09-18 23:36:57 +0200

    [edit] add dinamic grid difficulty

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,16 @@
-
 // 1. porto gli elementi su js come variabili e creo event list
 // 2. reset
 // 3. inserire div square da js e resettare quelle esistenti
-// 4. creare ciclo per numerare caselle 
+// 4. creare ciclo per numerare caselle
 // 5. inserire classe event listener con toggle su div square in modo da colorare lo sfondo
 // 6. stampare numeri visibili al click
-
-
+// 7. aggiungere difficoltà
 
 // 1.
-const btnPlayEl = document.querySelector('.btn-play');
-const containerEl = document.querySelector('._container');
-const mainEl = document.querySelector('main');
-const difficultySelectEl = document.querySelector('#difficulty-select');
+const btnPlayEl = document.querySelector(".btn-play");
+const containerEl = document.querySelector("._container");
+const mainEl = document.querySelector("main");
+const difficultySelectEl = document.querySelector("#difficulty-select");
 
 let limit;
 
@@ -20,44 +18,46 @@ let limit;
 // tolgo gli elementi all'interno del contenitore con funzione reset
 reset();
 
-
 // 3.
-btnPlayEl.addEventListener('click', function(){
+btnPlayEl.addEventListener("click", function () {
   //resetto il contenitore al click (evito la creazione di più contenitori)
   reset();
 
-  console.log('cliec')
+  console.log("cliec");
 
   // 4.
   for (let i = 1; i <= limit; i++) {
     const square = generateSquare(i);
     // 5.
-    square.addEventListener ('click', function (){
-      this.classList.toggle('clicked');
+    square.addEventListener("click", function () {
+      this.classList.toggle("clicked");
       console.log(this.innerText);
-    })
+    });
 
     containerEl.appendChild(square);
   }
-})
-
+});
 
 // FUNCTIONS
 
 function reset() {
-  containerEl.innerHTML = '';
+  containerEl.innerHTML = "";
 }
 
 // 3.
 function generateSquare(index) {
 
-  const newSquare = document.createElement('div');
+  const newSquare = document.createElement("div");
+
+  // dimensione dinamica degli square dentro il container
+  newSquare.style.width =`calc(100% / ${Math.sqrt(limit)})`;
+
   // do una classe al nuovo elemento creato
-  newSquare.className = 'square';
+  newSquare.className = "square";
   // Crea un elemento <span>
-  const span = document.createElement('span');
+  const span = document.createElement("span");
   // Imposta il testo dello span
-  span.innerText = index; 
+  span.innerText = index;
   // Aggiungi lo span alla casella
   newSquare.appendChild(span);
   return newSquare;
@@ -65,6 +65,5 @@ function generateSquare(index) {
 
 // funzione per cambiare la difficolta di gioco
 function changeDifficulty() {
-  return limit = Number(difficultySelectEl.value)
-  
+  return (limit = Number(difficultySelectEl.value));
 }
