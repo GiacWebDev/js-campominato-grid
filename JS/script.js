60381253cd695931bf55fada4b4045dc1af27f90
GiacWebDev <giacobechini@gmail.com>
2023-09-18 23:10:57 +0200

    [edit] difficulty level

--- JS/script.js
+++ JS/script.js
@@ -12,32 +12,33 @@
 const btnPlayEl = document.querySelector('.btn-play');
 const containerEl = document.querySelector('._container');
 const mainEl = document.querySelector('main');
+const difficultySelectEl = document.querySelector('#difficulty-select');
+
+let limit;
 
 // 2.
 // tolgo gli elementi all'interno del contenitore con funzione reset
 reset();
+
 
 // 3.
 btnPlayEl.addEventListener('click', function(){
   //resetto il contenitore al click (evito la creazione di più contenitori)
   reset();
 
-  
+  console.log('cliec')
+
   // 4.
-  for (let i = 1; i <= 100; i++) {
-  
+  for (let i = 1; i <= limit; i++) {
     const square = generateSquare(i);
-    
     // 5.
     square.addEventListener ('click', function (){
       this.classList.toggle('clicked');
       console.log(this.innerText);
     })
-  
+
     containerEl.appendChild(square);
   }
-
-  mainEl.classList.remove('d-none')
 })
 
 
@@ -60,4 +61,10 @@ function generateSquare(index) {
   // Aggiungi lo span alla casella
   newSquare.appendChild(span);
   return newSquare;
+}
+
+// funzione per cambiare la difficolta di gioco
+function changeDifficulty() {
+  return limit = Number(difficultySelectEl.value)
+  
 }
